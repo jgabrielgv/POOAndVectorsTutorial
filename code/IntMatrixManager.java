af874de199fb8e9954a7294883c6341a446e19fd
juancho <jgmezvargas@gmail.com>
2016-09-11 16:10:00 -0600

    Fixing bug in searchElement method

--- code/IntMatrixManager.java
+++ code/IntMatrixManager.java
@@ -81,7 +81,7 @@ public class IntMatrixManager {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (element == matrix[i][j]) {
-                    return matrix[i];
+                    return new int[] { i, j };
                 }
             }
         }
